09f540121fd96d7850d747c8b77778cc2c295e13
Yujun26 <aleck1314@hotmail.com>
2026-05-23 13:33:58 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,17 @@
+const CACHE_NAME = 'energy-flow-v1';
+const urlsToCache = [
+  '.',  // 当前目录下的HTML文件
+  'manifest.json'
+];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
